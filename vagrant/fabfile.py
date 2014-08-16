9a6801ff44e2e6a55c0e50aa701b6f244df6fe77
Nathan Fiedler <nathanfiedler@fastmail.fm>
2014-08-16 15:30:30 -0700

    Fabric code to install Erlang/OTP

--- vagrant/fabfile.py
+++ vagrant/fabfile.py
@@ -1,40 +1,72 @@
 # -*- coding: utf-8 -*-
 """Fabric file for installing requirements on OpenIndiana."""
 
-from fabric.api import env, sudo
+from fabric.api import cd, env, path, run, shell_env, sudo
 
 env.sudo_prefix = 'pfexec '
 
-# TOOD: isntall compress/xz so we can fetch the smallest source tarball
-# TODO: install Python 2.7
-# TODO: install pip using python2.7
-# TODO: install exifread using pip2.7
-# TODO: install Erlang/OTP
 # TODO: install CouchDB
+# TOOD?: install compress/xz so we can fetch the smallest source tarball
+# TODO?: install Python 2.7
+# TODO?: install pip using python2.7
+# TODO?: install exifread using pip2.7
+
+DIR_FOP = 'fop-1.1'
+TAR_FOP = '{}-bin.tar.gz'.format(DIR_FOP)
+URL_FOP = 'http://mirrors.sonic.net/apache/xmlgraphics/fop/binaries/{}'.format(TAR_FOP)
+DIR_OTP = 'otp_src_17.1'
+TAR_OTP = '{}.tar.gz'.format(DIR_OTP)
+URL_OTP = 'http://www.erlang.org/download/{}'.format(TAR_OTP)
+
+
+def install_erlang():
+    """Install Erlang/OTP from source tarball."""
+    install_tools()
+    install_jdk()
+    # Fetch and extract Apache FOP binary tarball
+    run('wget -q {}'.format(URL_FOP))
+    run('tar zxf {}'.format(TAR_FOP))
+    # Prepare to build Erlang/OTP from source
+    run('wget -q {}'.format(URL_OTP))
+    run('tar zxf {}'.format(TAR_OTP))
+    paths = '/opt/gcc/4.4.4/bin:~/{}'.format(DIR_FOP)
+    with cd(DIR_OTP), path(paths):
+        run('./configure')
+        run('make')
+        sudo('make install')
+        with shell_env(FOP_OPTS="-Xmx512m"):
+            run('make docs')
+            sudo('make install-docs')
+    run('rm -rf {}* {}*'.format(DIR_FOP, DIR_OTP))
 
 
 def install_tools():
     """Install the developer tools packages."""
-    _pkg_install('developer/illumos-gcc')
-    _pkg_install('developer/gnu-binutils')
-    _pkg_install('system/header')
-    _pkg_install('system/library/math/header-math')
-    _pkg_install('developer/library/lint')
-    _pkg_install('compatibility/ucb')
-    # TODO: how to change the PATH? only really need it to build Python
-    # $ export PATH=/opt/gcc/4.4.4/bin:$PATH
-
-
-def add_gcc_lib():
-    """Add the gcc lib to the crle path."""
+    tools_pkgs = [
+        'developer/illumos-gcc',
+        'developer/gnu-binutils',
+        'system/header',
+        'system/library/math/header-math',
+        'developer/library/lint',
+        'compatibility/ucb'
+    ]
+    _pkg_install(tools_pkgs)
+    # crle is idempotent, adding the same path twice is okay
     sudo('crle -u -l /opt/gcc/4.4.4/lib')
 
 
-def _pkg_install(pkg):
-    """Install the named package.
+def install_jdk():
+    """Install the Java Development Kit package."""
+    _pkg_install('developer/java/jdk')
 
-    :type pkg: str
-    :param pkg: name of package to install
+
+def _pkg_install(pkg):
+    """Install the named package or list of packages.
+
+    :type pkg: str|list
+    :param pkg: name(s) of package(s) to install
 
     """
+    if isinstance(pkg, list):
+        pkg = ' '.join(pkg)
     sudo("pkg install -q {}".format(pkg))
