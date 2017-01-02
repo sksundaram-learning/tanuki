820e788f52dc4c72928de5ddaf83f6d9a8da0e93
Nathan Fiedler <nathanfiedler@fastmail.fm>
2017-01-01 20:55:43 -0800

    Replace Phoenix text with Tanuki on index page

--- apps/tanuki_web/test/controllers/page_controller_test.exs
+++ apps/tanuki_web/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TanukiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Tanuki!"
   end
 end
