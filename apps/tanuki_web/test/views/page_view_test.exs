eb849eb7b80adad1c5698e2b056a9bbf8d385499
Nathan Fiedler <nathanfiedler@fastmail.fm>
2017-03-18 12:40:27 -0700

    Unit test for the page view functions

--- apps/tanuki_web/test/views/page_view_test.exs
+++ apps/tanuki_web/test/views/page_view_test.exs
@@ -1,3 +1,12 @@
 defmodule TanukiWeb.Web.PageViewTest do
   use TanukiWeb.Web.ConnCase, async: true
+
+  test "quicktime mimetype to mp4" do
+    assert TanukiWeb.Web.PageView.mimetype("video/quicktime") == "video/mp4"
+  end
+
+  test "other mimetypes returned as-is" do
+    assert TanukiWeb.Web.PageView.mimetype("video/ogg") == "video/ogg"
+    assert TanukiWeb.Web.PageView.mimetype("image/jpeg") == "image/jpeg"
+  end
 end
